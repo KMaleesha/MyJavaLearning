d37f0625fb624cde241f478d217a797a5063b2cc
KMaleesha <maleeshakumarasinghe@gmail.com>
2024-01-11 18:29:07 +0530

    operators

---
+++ src/Math.java
@@ -0,0 +1,28 @@
+public class Math {
+    public static void main(String[] args){
+
+        int x, y;
+        x = 10;
+        y = 34;
+
+        // addition
+        int addition = x+y;
+        System.out.println(addition);
+
+        // division
+        float division = y/x;
+        System.out.println(division);
+
+        // multiplication
+        int multi = x*y;
+        System.out.println(multi);
+
+        //modules
+        int modules = y%x;
+        System.out.println(modules);
+
+        // subtraction
+        int subtraction = y-x;
+        System.out.println(subtraction);
+    }
+}
---
+++ src/operators.java
@@ -0,0 +1,16 @@
+public class operators {
+
+    public static void main(String[] args){
+        int x = 10;
+        x = x + 1;
+        System.out.println(x);
+
+        //post increment
+        x++;
+        System.out.println(x);
+
+        //pre increment
+        ++x;
+        System.out.println(x);
+    }
+}
