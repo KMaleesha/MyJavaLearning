439c178ea295b1cd9d5626bc91617d1888b911ce
KMaleesha <maleeshakumarasinghe@gmail.com>
2024-01-11 19:03:41 +0530

    if else

---
+++ src/ifElse.java
@@ -0,0 +1,16 @@
+public class ifElse {
+    public static void main(String[] args){
+        int x = 50;
+        if (x == 10){
+            System.out.println("equal");
+        }else if(x > 10){
+            System.out.println("yes");
+        }else if (x< 10){
+            System.out.println("no");
+        }else if (x != 10) {
+            System.out.println("not equal");
+        }else{
+            System.out.println("none");
+        }
+    }
+}
