d1113eba91e294b654250ccd87bde39c3cbd3df5
KMaleesha <maleeshakumarasinghe@gmail.com>
2024-01-12 14:42:58 +0530

    loop

---
+++ src/Loop.java
@@ -0,0 +1,21 @@
+public class Loop  {
+    public static void main(String[] args){
+        int i = 0;
+        while (i<=20){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("-----------------");
+
+        int x = 0;
+        do{
+            System.out.println("Hi Maleesha");
+            x++;
+        }while(x<=10);
+        System.out.println("-----------------");
+
+        for(int a = 0; a<=10; a++){
+            System.out.println("Hi For Loop");
+        }
+    }
+}
