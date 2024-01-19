e3af8b90b431e3b9e0357bb8d5de694d8c384c33
KMaleesha <maleeshakumarasinghe@gmail.com>
2024-01-19 16:59:39 +0530

    phone class updated

--- src/inheritance/phone/newPhone.java
+++ src/inheritance/phone/newPhone.java
@@ -2,8 +2,12 @@ package inheritance.phone;
 
 public class newPhone extends  oldPhone{
 
+
     public static void main(String[]args){
         oldPhone myNewPhone = new oldPhone();
+        myNewPhone.model = "iPhone";
+        myNewPhone.color = "Gold";
+        myNewPhone.ram = 124;
         myNewPhone.makeCall();
         myNewPhone.browseInternet();
     }
