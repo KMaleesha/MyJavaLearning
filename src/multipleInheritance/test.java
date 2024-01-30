86991aad47a591ddd30ed594855cf9ac19c465f1
KMaleesha <maleeshakumarasinghe@gmail.com>
2024-01-30 20:30:04 +0530

    multiple inheritance

---
+++ src/multipleInheritance/test.java
@@ -0,0 +1,27 @@
+package multipleInheritance;
+
+class a{
+
+}
+class b{
+
+}
+interface d{
+
+}
+class c extends a implements d{
+
+}
+
+interface x{
+
+}
+interface y{
+
+}
+interface z extends x,y{
+
+}
+
+public class test implements x,y{
+}
