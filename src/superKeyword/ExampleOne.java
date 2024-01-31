635f01226a8dda6c57d45329dde52be533ef8425
KMaleesha <maleeshakumarasinghe@gmail.com>
2024-01-31 21:59:25 +0530

    supper keyword

--- src/superKeyword/ExampleOne.java
+++ src/superKeyword/ExampleOne.java
@@ -1,4 +1,20 @@
 package superKeyword;
 
+class TestOne{
+    public void print(){
+        System.out.println("Print Test one class");
+    }
+}
+
+class TestTwo extends TestOne{
+    public void print(){
+        super.print();
+        System.out.println("Print Test two class");
+    }
+}
 public class ExampleOne {
+    public static void main(String args[]){
+        TestTwo testTwo = new TestTwo();
+        testTwo.print();
+    }
 }
