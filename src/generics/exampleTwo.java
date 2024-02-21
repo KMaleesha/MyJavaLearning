6009446f4a20229ba39af57a182d6d94fc70fbf8
KMaleesha <maleeshakumarasinghe@gmail.com>
2024-02-21 13:46:22 +0530

    Generics

--- src/generics/exampleTwo.java
+++ src/generics/exampleTwo.java
@@ -1,4 +1,19 @@
 package generics;
 
-public class exampleTwo {
+public class exampleTwo<T> {
+
+    T[] arr = (T[]) new Object[3];
+    public static void main(String args[]){
+
+        exampleTwo<Integer> exampleTwo = new exampleTwo();
+        exampleTwo.arr[0] = 10;
+        exampleTwo.arr[1] = 34;
+        exampleTwo.arr[2] = 12;
+
+        int num;
+        for (int i = 0; i<3; i++){
+            num = exampleTwo.arr[i];
+            System.out.println(num);
+        }
+    }
 }
