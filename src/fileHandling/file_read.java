86c805a116599e209fad021044aa4c0dcf6c2713
KMaleesha <maleeshakumarasinghe@gmail.com>
2024-02-29 13:51:32 +0530

    file handling -> read

--- src/fileHandling/file_read.java
+++ src/fileHandling/file_read.java
@@ -1,4 +1,21 @@
 package fileHandling;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class file_read {
+    public static void main(String[] args) {
+        File file = new File("test2.txt");
+
+        try{
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()){
+                System.out.println(reader.nextLine());
+            }
+
+            reader.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
 }
